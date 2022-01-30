c1b7b40d62a1b6d1deb92fdde0b3cc826b3b27e7
star1 <star11a80n72@gmail.com>
2022-01-30 22:39:34 +0330

    better this programm

--- sample1.js
+++ sample1.js
@@ -1,3 +1,61 @@
+let result= document.querySelector(".result");
+let firstnumber=document.querySelector("#Firstnumber")
+let Untile=document.querySelector("#Untile")
+let btnresult = document.querySelector(".btn")
+let error =document.querySelector(".error")
+
+btnresult.addEventListener("click",e=>{
+    var FirstNumber=firstnumber.value;
+    var SecondNumber =Untile.value;
+
+
+    if(+SecondNumber> +FirstNumber){
+        function isPrime(FirstNumber,SecondNumber){
+            for(i=FirstNumber;i<=SecondNumber;i++){
+                let d = parseInt(i ** 0.5) + 1;
+                var c =true;
+                for (j = 2; j < d; j++) {
+                    if (i % j == 0) {
+                      c = false;
+                      break;
+                    }
+                  }
+                  if (c) {
+                    result.innerHTML +=`<p>_${i}</p>`;
+                  }
+            }
+        }
+        isPrime(+FirstNumber,+SecondNumber)
+        error.textContent="";
+    }else{
+        error.textContent = `Your second number must be big of first number`
+    }
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // let n  = 90 ; //numbers
@@ -33,25 +91,25 @@
 // console.log(n,isPrime);
 // =========================================
 
-function isPrime(n){
+// function isPrime(n){
 
-    let isPrime = true;
-    let i = 2 ;
-    while(i < parseInt(n**0.5)+1){
-        if(n % i == 0 ){
-            isPrime = false ;
-            break;
-        }
-        i++;
-    }
-    return isPrime
-}
-let a = 8 ;
-let b = 70 ; 
-let c = 100007
-console.log(a,isPrime(a))
-console.log(b,isPrime(b))
-console.log(c,isPrime(c))
+//     let isPrime = true;
+//     let i = 2 ;
+//     while(i < parseInt(n**0.5)+1){
+//         if(n % i == 0 ){
+//             isPrime = false ;
+//             break;
+//         }
+//         i++;
+//     }
+//     return isPrime
+// }
+// let a = 8 ;
+// let b = 70 ; 
+// let c = 100007
+// console.log(a,isPrime(a))
+// console.log(b,isPrime(b))
+// console.log(c,isPrime(c))
 
 
 
